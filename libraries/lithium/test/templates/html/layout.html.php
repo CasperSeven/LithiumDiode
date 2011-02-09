2930687e2d3a2ad9643c23192736deed72413ce0
David Persson <davidpersson@gmx.de>
2011-02-10 00:48:06 +0100

    Adding header to test layout file.

--- libraries/lithium/test/templates/html/layout.html.php
+++ libraries/lithium/test/templates/html/layout.html.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2010, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+?>
+<?php
 	use lithium\util\Inflector;
 	$base = $request->env('base');
 ?>
