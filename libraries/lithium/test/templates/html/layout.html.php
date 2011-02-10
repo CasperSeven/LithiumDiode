3e6a206af609b90ea5eff35f0a1caa460d591539
David Persson <davidpersson@gmx.de>
2011-02-10 13:13:35 +0100

    Adding comment to test layout title tag, clarifying intention why it was left blank.

--- libraries/lithium/test/templates/html/layout.html.php
+++ libraries/lithium/test/templates/html/layout.html.php
@@ -12,6 +12,7 @@ use lithium\util\Inflector;
 <!doctype html>
 <html>
 	<head>
+		<!-- Title intentionally left blank, forcing user agents use the current URL as title. -->
 		<title></title>
 		<?php $base = $request->env('base'); ?>
 		<meta charset="utf-8" />
